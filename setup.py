8e8e3b9a2f5b35d9d4604b998e1544fd17d840e3
michaelb <michaelpb@gmail.com>
2017-11-21 17:10:08 -0800

    fixing setup.py

--- setup.py
+++ setup.py
@@ -25,21 +25,30 @@ version = get_version("django_view_hierarchy", "__init__.py")
 
 
 if sys.argv[-1] == 'publish':
-    try:
-        import wheel
-        print("Wheel version: ", wheel.__version__)
-    except ImportError:
-        print('Wheel library missing. Please run "pip install wheel"')
-        sys.exit()
     os.system('python setup.py sdist upload')
-    os.system('python setup.py bdist_wheel upload')
     sys.exit()
 
-if sys.argv[-1] == 'tag':
-    print("Tagging the version on git:")
-    os.system("git tag -a %s -m 'version %s'" % (version, version))
-    os.system("git push --tags")
+if sys.argv[-1] == 'test':
+    os.system('py.test')
     sys.exit()
+
+
+#if sys.argv[-1] == 'publish':
+#    try:
+#        import wheel
+#        print("Wheel version: ", wheel.__version__)
+#    except ImportError:
+#        print('Wheel library missing. Please run "pip install wheel"')
+#        sys.exit()
+#    os.system('python setup.py sdist upload')
+#    os.system('python setup.py bdist_wheel upload')
+#    sys.exit()
+#
+#if sys.argv[-1] == 'tag':
+#    print("Tagging the version on git:")
+#    os.system("git tag -a %s -m 'version %s'" % (version, version))
+#    os.system("git push --tags")
+#    sys.exit()
 
 readme = open('README.rst').read()
 history = open('HISTORY.rst').read().replace('.. :changelog:', '')
@@ -57,23 +66,19 @@ setup(
     ],
     include_package_data=True,
     install_requires=[],
-    license="Apache Software License 2.0",
+    license="GNU General Public License v3 or later (GPLv3+)",
     zip_safe=False,
     keywords='django_view_hierarchy',
     classifiers=[
         'Development Status :: 3 - Alpha',
-        'Framework :: Django',
-        'Framework :: Django :: 1.8',
-        'Framework :: Django :: 1.9',
-        'Framework :: Django :: 1.10',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
+        'Framework :: Django',
+        'Framework :: Django :: 1.10',
+        'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: '
+        'GNU General Public License v3 or later (GPLv3+)',
     ],
 )
